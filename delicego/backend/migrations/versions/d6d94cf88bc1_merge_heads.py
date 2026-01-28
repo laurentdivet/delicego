43650afea2f9f76b7562b977d62336f1dfdef0e9
laurentdivet <laurentdivet@icloud.com>
2026-01-28 14:03:09 +0100

    alembic: merge heads

---
+++ delicego/backend/migrations/versions/d6d94cf88bc1_merge_heads.py
@@ -0,0 +1,28 @@
+"""merge heads
+
+Revision ID: d6d94cf88bc1
+Revises: 2b5f0e1a4d11, 9c1c1ab7b2c1
+Create Date: 2026-01-28 14:02:03.930129
+
+"""
+
+from __future__ import annotations
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+# revision identifiers, used by Alembic.
+revision = 'd6d94cf88bc1'
+down_revision = ('2b5f0e1a4d11', '9c1c1ab7b2c1')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
